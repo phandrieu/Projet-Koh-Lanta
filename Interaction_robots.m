009acd107c1af5ac0b74bf615af7f3c51b42b262
Paul-Henri Andrieu <paul-henri@phandrieu.fr>
2024-05-28 10:37:52 +0200

    Backup

--- Interaction_robots.m
+++ Interaction_robots.m
@@ -8,6 +8,49 @@ CG=0.05;
 
 F=[0;0;0];
 
+if J==1
+    for k=1:N1
+        if not(k == I)
+                l0=5*(X1(:,I)+Size1(k));
+                normale=(X1(:,I)-X1(:,k))/l0;
+                d=norm(normale);
+            
+            F= F + CO*normale*(d^(-3)-d^(-2))*exp(-d);
+        end
+    end
+    for k=1:N2
+        l0=5*(X1(:,I)+Size1(k));
+        normale=(X1(:,I)-X2(:,k))/l0;
+        d=norm(normale);
+            
+        F= F + CO*normale*(-d^(-2))*exp(-d);
+    end
+
+    F=F+ CG*(Gmasse-X(:,I))/norm(Gmasse-X(:,I))/N;
+
+else
+    for k=1:N2
+        if not(k == I)
+                l0=5*(X2(:,I)+Size2(k));
+                normale=(X2(:,I)-X2(:,k))/l0;
+                d=norm(normale);
+            
+            F= F + CO*normale*(d^(-3)-d^(-2))*exp(-d);
+        end
+    end
+    for k=1:N1
+        l0=5*(X1(:,I)+Size1(k));
+        normale=(X1(:,I)-X2(:,k))/l0;
+        d=norm(normale);
+            
+        F= F + CO*normale*(-d^(-2))*exp(-d);
+    end
+
+    F=F+ CG*(Gmasse-X(:,I))/norm(Gmasse-X(:,I))/N;
+
+
+
+
 for l=1:2
     for k=1:eval(['N', num2str(l)])
         if l==J
@@ -25,8 +68,7 @@ for l=1:2
             
             F= F + CO*normale*(-d^(-2))*exp(-d);
         end
-    end
-
     F=F+ CG*(Gmasse-X(:,I))/norm(Gmasse-X(:,I))/N;
+    end
 end
 end
