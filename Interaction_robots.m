3270932501333e7faf43f5e02aa5f2cc86516c71
Paul-Henri Andrieu <paul-henri@phandrieu.fr>
2024-05-28 10:43:04 +0200

    Sauvegarde

--- Interaction_robots.m
+++ Interaction_robots.m
@@ -6,7 +6,7 @@ function F=Interaction_robots(I,J,X1,X2,Gmasse,Size1,Size2,N1,N2)
 CO=0.5;
 CG=0.05;
 
-F=[0;0;0];
+F=[0;0];
 
 if J==1
     for k=1:N1
@@ -26,7 +26,7 @@ if J==1
         F= F + CO*normale*(-d^(-2))*exp(-d);
     end
 
-    F=F+ CG*(Gmasse-X(:,I))/norm(Gmasse-X(:,I))/N;
+    F=F+ CG*(Gmasse-X1(:,I))/norm(Gmasse-X1(:,I))/N1;
 
 else
     for k=1:N2
@@ -39,36 +39,13 @@ else
         end
     end
     for k=1:N1
-        l0=5*(X1(:,I)+Size1(k));
-        normale=(X1(:,I)-X2(:,k))/l0;
+        l0=5*(X2(:,I)+Size2(k));
+        normale=(X2(:,I)-X1(:,k))/l0;
         d=norm(normale);
             
         F= F + CO*normale*(-d^(-2))*exp(-d);
     end
 
-    F=F+ CG*(Gmasse-X(:,I))/norm(Gmasse-X(:,I))/N;
-
-
-
-
-for l=1:2
-    for k=1:eval(['N', num2str(l)])
-        if l==J
-            if not(k == I)
-                l0=5*(eval(['Size', num2str(l)])(I)+eval(['Size', num2str(l)])(k));
-                normale=(eval(['X', num2str(l)])(:,I)-eval(['X', num2str(l)])(:,k))/l0;
-                d=norm(normale);
-            
-            F= F + CO*normale*(d^(-3)-d^(-2))*exp(-d);
-            end
-        else
-            l0=5*(eval(['Size', num2str(l)])(I)+eval(['Size', num2str(l)])(k));
-            normale=(eval(['X', num2str(l)])(:,I)-eval(['X', num2str(l)])(:,k))/l0;
-            d=norm(normale);
-            
-            F= F + CO*normale*(-d^(-2))*exp(-d);
-        end
-    F=F+ CG*(Gmasse-X(:,I))/norm(Gmasse-X(:,I))/N;
-    end
+    F=F+ CG*(Gmasse-X2(:,I))/norm(Gmasse-X2(:,I))/N2;
 end
 end
